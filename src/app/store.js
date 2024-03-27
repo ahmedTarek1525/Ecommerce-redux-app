9d36f9bcf30ecb5852586895005151b315a7d443
Ahmed Tarek <160412976+ahmedTarek1525@users.noreply.github.com>
2024-03-27 05:54:59 +0200

    create slices folder, add sliderSlice & sliderReducer.

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import sliderReducer from "../features/slices/sliderSlice";
 
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+        slder: sliderReducer,
+    },
 })
