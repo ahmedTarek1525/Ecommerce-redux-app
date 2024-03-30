86cf0f5d4c12a39bf9ab9dc66335e9ba67070754
Ahmed Tarek <160412976+ahmedTarek1525@users.noreply.github.com>
2024-03-30 18:01:12 +0200

    add filteredProducts & productsSlice + configureStore and reducers.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
 import Main from './components/Main/Main';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import FilteredProducts from './components/FilteredProducts/FilteredProducts';
 
 function App() {
   return (
     <div className="App">
-      <Main></Main>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Main></Main>}></Route>
+          <Route path="/filteredProducts/:type" element={<FilteredProducts></FilteredProducts>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/NavigateButtons/NavigateButtons.jsx
+++ src/components/NavigateButtons/NavigateButtons.jsx
@@ -1,19 +1,27 @@
 import React from 'react'
 import { Button } from '@material-tailwind/react';
 import clothes from '../../assets/images/clothes.jpg';
+import { filteredProducts } from '../../features/slices/productsSlice';
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 function NavigateButtons() {
 
     const buttons = ["T-shirts" , "Hoodies" , "Jeans" ,"Dresses" , "Suits" , "Shoes" , "Jackets" , "bags"];
+
+    const dispatch = useDispatch();
 
   return (
     <div>
       <div className='flex items-center justify-center py-8'>
         {buttons.map((button, index) => {
             return <div key={index} className='mr-4'>
-               <Button color='gray' size='lg' variant='outlined' ripple={true} className='hover:bg-green-300 duration-300 ease-in-out'>
+              <Link to={"/filteredProducts/" + button}>
+               <Button color='gray' size='lg' variant='outlined' ripple={true} className='hover:bg-green-300 duration-300 ease-in-out'
+               onClick={() => dispatch(filteredProducts(button))} >
                 {button}
                 </Button> 
+              </Link>
             </div>
         })}
       </div>
