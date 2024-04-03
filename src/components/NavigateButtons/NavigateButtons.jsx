459482eae104aa16e38cdb3261e6653a652c0785
Ahmed Tarek <160412976+ahmedTarek1525@users.noreply.github.com>
2024-04-03 02:53:58 +0200

    CSS changes to the buttons and banner.

--- src/components/NavigateButtons/NavigateButtons.jsx
+++ src/components/NavigateButtons/NavigateButtons.jsx
@@ -17,7 +17,7 @@ function NavigateButtons() {
         {buttons.map((button, index) => {
             return <div key={index} className='mr-4'>
               <Link to={"/filteredProducts/" + button}>
-               <Button color='gray' size='lg' variant='outlined' ripple={true} className='hover:bg-green-300 duration-300 ease-in-out'
+               <Button color='gray' size='lg' variant='outlined' ripple={true} className='hover:bg-gray-300 duration-300 ease-in-out'
                onClick={() => dispatch(filteredProducts(button))} >
                 {button}
                 </Button> 
@@ -25,8 +25,8 @@ function NavigateButtons() {
             </div>
         })}
       </div>
-      <div className='bg-green-300 p-2 w-[55%] mx-auto rounded-md'>
-        <h3 className='text-orange-900 text-center text-lg front-inter font-bold tracking-normal leading-none'>SALES UP TO 50%</h3>
+      <div className='bg-black p-2 w-[55%] mx-auto rounded-md'>
+        <h3 className='text-red-600 text-center text-lg font-inter font-bold tracking-normal leading-none'>SALES UP TO 50%</h3>
       </div>
       <div className='flex justify-center items-center py-4'>
         <img className='h-[600px] w-[70%] rounded-md shadow-lg shadow-gray-600' src={clothes} alt='clothes'></img>
