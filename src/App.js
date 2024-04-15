7427e7a6ae84ba4fe3d170fb0301fecc088a2652
Ahmed Tarek <160412976+ahmedTarek1525@users.noreply.github.com>
2024-04-15 15:07:35 +0200

    Add authSlice logic.

--- src/App.js
+++ src/App.js
@@ -5,14 +5,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import FilteredProducts from './components/FilteredProducts/FilteredProducts';
 import SingleProducts from './components/FilteredProducts/SingleProducts';
 import Login from './components/Login/Login';
+import { useSelector} from "react-redux";
 
 function App() {
+  const user = useSelector((state) => state.user.user);
+  const {authUser} = user;
+  console.log ("user" , user);
+  console.log ("authUser" , authUser);
 
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Main></Main>}></Route>
+          <Route path='/' element={authUser ? <Main></Main> : <Login></Login> }></Route>
           <Route path='/login' element={<Login></Login>}></Route>
           <Route path="/filteredProducts/:type" element={<FilteredProducts></FilteredProducts>}></Route>
           <Route path="/filteredProducts/:type/:id" element={<SingleProducts></SingleProducts>}></Route>
